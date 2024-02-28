20a90e085017d9e8980706af432c7a0df6cb444c
Cerebrocyber <132948746+Cerebrocyber@users.noreply.github.com>
2024-02-27 23:41:20 -0300

    Create index.tsx

---
+++ src/pages/teste/index.tsx
@@ -0,0 +1,63 @@
+import { Inter } from "next/font/google";
+import styles from '../styles/pasteldobairro.module.css';
+import Image from "next/image";
+import Link from "next/link";
+
+const Pasteldobairro = () => (
+  <div className='homepastel'>
+    <div className="homepastel-background"></div>
+    <section className='homepastel-content'>
+      
+      
+
+      <div className="pastelmenu">
+  <div className="image-container">
+    <Image
+      src='/assets/images/pastelmenu1.webp'
+      alt=''
+      width={700}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+    <Image
+      src='/assets/images/pastelmenu2.webp'
+      alt=''
+      width={650}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+    <Image
+      src='/assets/images/pastelmenu3.webp'
+      alt=''
+      width={650}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+    <Image
+      src='/assets/images/pastelmenu4.webp'
+      alt=''
+      width={650}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+    <Image
+      src='/assets/images/pastelmenu5.webp'
+      alt=''
+      width={650}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+    <Image
+      src='/assets/images/pastelmenu6.webp'
+      alt=''
+      width={650}
+      height={150} /* Defina a altura desejada */
+      className='pastelmenu-image'
+    />
+  </div>
+</div>
+    </section>
+  </div>
+);
+
+export default Pasteldobairro;
