81468b0ccf99ed1a52a1bcbba35ba9231a6c89a5
Cerebrocyber <132948746+Cerebrocyber@users.noreply.github.com>
2024-02-27 23:29:36 -0300

    Update index.tsx

--- src/pages/pasteldobairro/index.tsx
+++ src/pages/pasteldobairro/index.tsx
@@ -70,7 +70,7 @@ const Pasteldobairro = () => (
       <div className="pastelmenu">
   <div className="image-container">
     <Image
-      src='/assets/images/pastelmenu1.webp'
+      src='https://res.cloudinary.com/do3pkg9to/image/upload/v1709087256/pastelmenu1_iw4lnp.webp'
       alt=''
       width={700}
       height={150} /* Defina a altura desejada */
