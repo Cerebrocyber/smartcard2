fd755618858af6d28cfa3f84c04bd87ea641ce46
Cerebrocyber <132948746+Cerebrocyber@users.noreply.github.com>
2024-03-01 17:34:20 -0300

    ok cru

--- src/pages/pastelariacuruzu/index.tsx
+++ src/pages/pastelariacuruzu/index.tsx
@@ -19,7 +19,7 @@ const Homecuruzu = () => (
       </p>
       <br className='max-md:hidden' />
       <div className="bots">
-        <Link href='tel:+551438133975' className='flex gap-2 flex-center'>
+        <Link href='tel:+5514996814646' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/bx1.svg'
             alt=''
@@ -28,7 +28,7 @@ const Homecuruzu = () => (
             className='bot'
           />
         </Link>
-        <Link href='https://api.whatsapp.com/send?phone=5514996232860&text=Gostaria%20de%20fazer%20um%20pedido!' className='flex gap-2 flex-center'>
+        <Link href='https://api.whatsapp.com/send?phone=5514996814646&text=Gostaria%20de%20fazer%20um%20pedido!' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/bx2.svg'
             alt=''
@@ -37,7 +37,7 @@ const Homecuruzu = () => (
             className='bot'
           />
         </Link>
-        <Link href='https://www.facebook.com/pasteldobairro/' className='flex gap-2 flex-center'>
+        <Link href='https://www.facebook.com/profile.php?id=100082903793737' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/bx3.svg'
             alt=''
@@ -46,7 +46,7 @@ const Homecuruzu = () => (
             className='bot'
           />
         </Link>
-        <Link href='https://www.instagram.com/pasteldobairrobtu/' className='flex gap-2 flex-center'>
+        <Link href='https://www.instagram.com/pastelaria_curuzu/' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/bx4.svg'
             alt=''
@@ -55,7 +55,7 @@ const Homecuruzu = () => (
             className='bot'
           />
         </Link>
-        <Link href='https://www.google.com/maps/dir//Av.+Leonardo+Vilas+Boas,+2741+-+Vila+Nova+Botucatu,+Botucatu+-+SP,+18608-227/@-22.8684529,-48.5404603,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x94c7209610ba62b1:0xfd982e6c798107e9!2m2!1d-48.4580587!2d-22.8684741?entry=ttu' className='flex gap-2 flex-center'>
+        <Link href='https://www.google.com/maps?sca_esv=d58752900c71090e&output=search&q=Rua+Curuzu,+512+-+Centro+-+Botucatu/SP&source=lnms&entry=mc' className='flex gap-2 flex-center'>
           <Image
             src='/assets/images/bx5.svg'
             alt=''
